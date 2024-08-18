0cdc9771f5be1be4ab57b3e460cfd359b66d14cf
Seb <sebmarsontk@gmail.com>
2024-08-18 23:28:48 +0100

    Fixed help screen button

--- scenes/help_screen.gd
+++ scenes/help_screen.gd
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_back_button_pressed() -> void:
+func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
